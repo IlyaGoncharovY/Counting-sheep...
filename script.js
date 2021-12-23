30aaf39fa085cf233f3a8b1c9e313a1fe0cbfd76
Ilya <goncharov.i@rambler.ru>
2021-12-23 21:12:23 +0300

    test ok

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function countSheeps(arrayOfSheep) {
-// Consider an array/list of sheep where some
-//  sheep may be missing from their place. 
-//  We need a function that counts
-//  the number of sheep present in the array (true means present).
+    let present = arrayOfSheep.filter(sheep => sheep);
+    return present.length;
 }
